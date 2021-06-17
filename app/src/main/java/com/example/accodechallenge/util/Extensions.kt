dc4ddde0d2c45f4ee6212fc0294edbf40eb12b3d
Steven Gerdes <steven.gerdes@tackmobile.com>
2021-06-16 22:22:40 -0500

    add hide keyboard extension function.

--- app/src/main/java/com/example/accodechallenge/util/Extensions.kt
+++ app/src/main/java/com/example/accodechallenge/util/Extensions.kt
@@ -1,4 +1,20 @@
 package com.example.accodechallenge.util
 
+import android.app.Activity
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+
 val <T> T.exhaustive: T
     get() = this
+
+fun Activity?.tryHideKeyboardIfVisible() {
+    try {
+        val inputMethodManager = this?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val currentFocusedView = currentFocus
+        currentFocusedView?.let {
+            inputMethodManager.hideSoftInputFromWindow(currentFocusedView.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+        }
+    } catch (e: Throwable) {
+        // not much we can do here
+    }
+}
